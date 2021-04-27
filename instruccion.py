3ef31c74c40141961ebfcf11dc8584eacaab5dc3
diegolacomba <diegolacomba@gmail.com>
2021-04-27 12:01:02 +0200

    cosillas

---
+++ instruccion.py
@@ -0,0 +1,9 @@
+class Instrucciones:
+    def __init__(self, cod, rd, rs, rt, inm):
+        self.cod = cod
+        self.rd = rd
+        self.rs = rs
+        self.rt = rt
+        self.inm = inm
+
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# CODIGOS DE OPERACION
 global cod_ADD
 global cod_SUB
 global cod_LW
@@ -9,6 +10,34 @@ cod_SUB = 2
 cod_LW = 3
 cod_SW = 4
 cod_MULT = 5
+
+global size_REG # NUMERO REGISTROS
+global size_DAT # TAMAÑO MEMORIA DATOS
+global size_INS # TAMAÑO MEMORIA INSTRUCCIONES
+
+size_REG = 16
+size_DAT = 32
+size_INS = 32
+
+# CODIGOS UNIDADES FUNCIONALES
+global TOTAL_UF
+global ALU
+global MEM
+global MULT
+
+TOTAL_UF = 3
+ALU = 0
+MEM = 1
+MULT = 2
+
+# CICLOS DE EJECUCION POR TIPO DE INSTRUCCION
+global ciclos_MEM
+global ciclos_ALU
+global ciclos_MULT
+
+ciclos_MEM = 2
+ciclos_ALU = 1
+ciclos_MULT = 5
 
 
 
