835b763e400b6449e8b5ce0071654574379a3c00
diegolacomba <diegolacomba@gmail.com>
2021-04-27 11:22:35 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+global cod_ADD
+global cod_SUB
+global cod_LW
+global cod_SW
+global cod_MULT
+
+cod_ADD = 1
+cod_SUB = 2
+cod_LW = 3
+cod_SW = 4
+cod_MULT = 5
+
+
+
+
+if __name__ == '__main__':
+    print("Hello")
